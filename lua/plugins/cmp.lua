7403fb7c2fe599ca28e0cfca908e1d9ab126df80
runharu0662 <haruta141@icloud.com>
2025-05-23 23:50:18 +0900

    hoge

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,22 +1,22 @@
 return {
-	"hrsh7th/nvim-cmp",
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-	},
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+    },
 
-	config = function()
-		local cmp = require("cmp")
-		local map = cmp.mapping
+    config = function()
+        local cmp = require("cmp")
+        local map = cmp.mapping
 
-		cmp.setup({
-			mapping = map.preset.insert({
-				["<C-n>"] = map.complete(),
-				["<CR>"] = map.confirm({ select = false }),
-			}),
-		})
+        cmp.setup({
+            mapping = map.preset.insert({
+                ["<C-n>"] = map.complete(),
+                ["<CR>"] = map.confirm({ select = false }),
+            }),
+        })
 
-		vim.lsp.config("*", {
-			capabilities = require("cmp_nvim_lsp").default_capabilities(),
-		})
-	end,
+        vim.lsp.config("*", {
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+        })
+    end,
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,5 +1,13 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = "nvim-lua/plenary.nvim",
-	config = true,
+  "nvimtools/none-ls.nvim",
+  dependencies = "nvim-lua/plenary.nvim",
+  config = function()
+    local null_ls = require("null-ls") -- ← none-ls はモジュール名まだ "null-ls"
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.prettier,
+
+      },
+    })
+  end,
 }
