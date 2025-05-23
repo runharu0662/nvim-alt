3fdd10d39ce52b80d096c68996430151b62032f9
runharu0662 <haruta141@icloud.com>
2025-05-24 00:09:56 +0900

    hoge

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,13 +1,5 @@
 return {
   "nvimtools/none-ls.nvim",
   dependencies = "nvim-lua/plenary.nvim",
-  config = function()
-    local null_ls = require("null-ls") -- ← none-ls はモジュール名まだ "null-ls"
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.prettier,
-
-      },
-    })
-  end,
+  config = true,
 }
