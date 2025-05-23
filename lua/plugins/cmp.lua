b2087e5f0f781acfab70f7344fb418cd0dab8e98
runharu0662 <haruta141@icloud.com>
2025-05-23 18:05:49 +0900

    re

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -13,8 +13,8 @@ return {
 			})
 	},
 	config = function()
-			vim.lsp.config("*", {
-				capabilities = require("cmp_nvim_lsp").default_capabilities(),
-			}),
+		vim.lsp.config("*", {
+			capabilities = require("cmp_nvim_lsp").default_capabilities(),
+		})
 	end,
 }
