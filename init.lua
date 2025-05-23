e69f50592ae8e182706954952dad565131f6f3cb
runharu0662 <haruta141@icloud.com>
2025-05-23 20:48:06 +0900

    re

--- init.lua
+++ init.lua
@@ -3,5 +3,11 @@ require("mappings")
 require("options")
 vim.cmd([[colorscheme tokyonight-storm]])
 require("user.ui")
-vim.o.laststatus = 3
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    vim.o.laststatus = 3
+  end,
+})
 
