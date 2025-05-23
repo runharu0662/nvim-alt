fff603df8e97b50158e118274ef15e25246f0a75
runharu0662 <haruta141@icloud.com>
2025-05-24 00:08:34 +0900

    hoge

--- lua/plugins/mason-null-ls.lua
+++ lua/plugins/mason-null-ls.lua
@@ -3,11 +3,29 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
     "williamboman/mason.nvim",
-    "nvimtools/none-ls.nvim", -- ← 最新名
+    "nvimtools/none-ls.nvim",
   },
-  opts = {
-    automatic_setup = true,
-    handlers = {},
-  },
+  config = function()
+    local null_ls = require("none-ls") -- ← ここが重要！
+
+    null_ls.setup({})
+
+    require("mason-null-ls").setup({
+      automatic_installation = true,
+      ensure_installed = {
+        "stylua",
+        "prettier",
+      },
+      handlers = {
+        function() end, -- 全自動登録を無効化
+        stylua = function()
+          null_ls.register(null_ls.builtins.formatting.stylua)
+        end,
+        prettier = function()
+          null_ls.register(null_ls.builtins.formatting.prettier)
+        end,
+      },
+    })
+  end,
 }
 
