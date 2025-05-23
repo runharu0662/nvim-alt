9c6eb56615c072ff9ea85dc404b43765289fd060
runharu0662 <haruta141@icloud.com>
2025-05-23 18:20:05 +0900

    nvim-cmp-setup

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,18 +3,23 @@ return {
 	dependences = {
 		"hrsh7th/cmp-nvim-lsp",
 	},
-	opts = {
-			mapping = {
-				["<C-n>"] = cmp.mapping.complete(),
-				["<CR>"] = map.confirm({ select = false }),
-			}
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-			})
-	},
 	config = function()
-		vim.lsp.config("*", {
+		
+    local cmp = require 'cmp'
+    local map = cmp.mapping
+
+    cmp.setup {
+      mapping = map.preset.insert {
+        ['<C-d>'] = map.scroll_docs(-4),
+        ['<C-f>'] = map.scroll_docs(4),
+        ['<C-Space>'] = map.complete(),
+        ['<C-e>'] = map.abort(),
+        ['<CR>'] = map.confirm { select = false },
+      },
+    }
+
+	vim.lsp.config("*", {
 			capabilities = require("cmp_nvim_lsp").default_capabilities(),
-		})
+		}),
 	end,
 }
