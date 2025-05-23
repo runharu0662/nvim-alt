f8ac94f18c2deda6905d1d4b5fb1af0ade2d18f4
runharu0662 <haruta141@icloud.com>
2025-05-23 17:36:36 +0900

    change mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,3 +1,9 @@
+-- move between windows
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to below window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to above window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+
 -- neotree
 vim.keymap.set("n", "<space>e", "<cmd>Neotree toggle<CR>", { desc = "Neo-tree toggle" })
 
