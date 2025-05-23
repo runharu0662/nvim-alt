b68fee4da78c0330fee697b7776be8558beabe9e
runharu0662 <haruta141@icloud.com>
2025-05-23 13:57:46 +0900

    change ufo settigs

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -3,5 +3,8 @@ return {
 	dependencies = { "kevinhwang91/promise-async" },
 	config = function()
 		require("ufo").setup()
+		vim.foldenable = false
+		vim.o.foldlevel = 99
+		vim.o.foldcolumn = "1"
 	end,
 }
