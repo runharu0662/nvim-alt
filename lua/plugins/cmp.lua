ac309ff35516d2c13fa4ef73726403df64d31126
runharu0662 <haruta141@icloud.com>
2025-05-23 18:22:09 +0900

    refix

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -4,22 +4,18 @@ return {
 		"hrsh7th/cmp-nvim-lsp",
 	},
 	config = function()
-		
-    local cmp = require 'cmp'
-    local map = cmp.mapping
+		local cmp = require("cmp")
+		local map = cmp.mapping
 
-    cmp.setup {
-      mapping = map.preset.insert {
-        ['<C-d>'] = map.scroll_docs(-4),
-        ['<C-f>'] = map.scroll_docs(4),
-        ['<C-Space>'] = map.complete(),
-        ['<C-e>'] = map.abort(),
-        ['<CR>'] = map.confirm { select = false },
-      },
-    }
+		cmp.setup({
+			mapping = map.preset.insert({
+				["<C-n>"] = map.complete(),
+				["<CR>"] = map.confirm({ select = false }),
+			}),
+		})
 
-	vim.lsp.config("*", {
+		vim.lsp.config("*", {
 			capabilities = require("cmp_nvim_lsp").default_capabilities(),
-		}),
+		})
 	end,
 }
