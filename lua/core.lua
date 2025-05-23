ad0c9cecf1060a637d2ad5b536e29875b929ee6c
runharu0662 <haruta141@icloud.com>
2025-05-23 14:33:41 +0900

    base

---
+++ lua/core.lua
@@ -0,0 +1,10 @@
+-- expandtab
+vim.opt.expandtab = true
+-- tabstop
+vim.opt.tabstop = 4
+-- softtabstop
+vim.opt.softtabstop = 4
+-- shiftwidth
+vim.opt.shiftwidth = 4
+-- number
+vim.opt.number = true
