3c11a57a7eced67a09e01609a4eae0b487b18b89
runharu0662 <haruta141@icloud.com>
2025-05-23 19:54:59 +0900

    re

--- init.lua
+++ init.lua
@@ -3,21 +3,49 @@ require("mappings")
 require("options")
 vim.cmd([[colorscheme tokyonight-storm]])
 
--- 背景透過用ハイライトグループのオーバーライド
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
-vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
-vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
-vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "none" })
-vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none" })
-vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "none" })
-vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "none" })
-vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "none" })
-vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { bg = "none" })
-vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
-vim.api.nvim_set_hl(0, "WinSeparator", { bg = "none" })
-vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+
+-- ベース
+local hl = vim.api.nvim_set_hl
+local none = { bg = "none" }
+
+-- 基本UI
+hl(0, "Normal", none)
+hl(0, "NormalNC", none)
+hl(0, "NormalFloat", none)
+hl(0, "FloatBorder", none)
+hl(0, "SignColumn", none)
+hl(0, "EndOfBuffer", none)
+hl(0, "StatusLine", none)
+hl(0, "WinSeparator", none)
+
+-- Telescope
+hl(0, "TelescopeNormal", none)
+hl(0, "TelescopeBorder", none)
+hl(0, "TelescopePromptNormal", none)
+hl(0, "TelescopePromptBorder", none)
+hl(0, "TelescopeResultsNormal", none)
+hl(0, "TelescopeResultsBorder", none)
+hl(0, "TelescopePreviewNormal", none)
+hl(0, "TelescopePreviewBorder", none)
+
+-- Neo-tree
+hl(0, "NeoTreeNormal", none)
+hl(0, "NeoTreeNormalNC", none)
+hl(0, "NeoTreeEndOfBuffer", none)
+hl(0, "NeoTreeFloatBorder", none)
+hl(0, "NeoTreeWinSeparator", none)
+
+-- Cokeline
+hl(0, "CokelineTab", none)
+hl(0, "CokelineTabSelected", none)
+hl(0, "CokelineBackground", none)
+hl(0, "CokelineBufferVisible", none)
+hl(0, "CokelineBufferSelected", none)
+hl(0, "CokelineBufferHidden", none)
+
+-- 透明が必要そうなもの
+hl(0, "VertSplit", none)
+hl(0, "TabLineFill", none)
+hl(0, "TabLine", none)
+hl(0, "TabLineSel", none)
 
