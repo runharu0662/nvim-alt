50c92c7d4127f7c3428274ff565ff43ff8cb12a6
runharu0662 <haruta141@icloud.com>
2025-05-23 16:19:27 +0900

    telescope

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,2 +1,8 @@
 -- neotree
 vim.keymap.set("n", "<space>e", "<cmd>Neotree toggle<CR>", { desc = "Neo-tree toggle" })
+
+-- telescope
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
