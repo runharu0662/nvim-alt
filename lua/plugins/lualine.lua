e83ccb7f3d94fe13b3b67a475de88518e69f1cbd
runharu0662 <haruta141@icloud.com>
2025-05-23 17:19:54 +0900

    delete lsp on lualine.lua

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -8,16 +8,5 @@ return {
 				theme = "dracula",
 				globalstatus = true,
 			},
-			sections = {
-				lualine_c = {
-					{
-						"diagnostics",
-						sources = { "nvim_diagnostic", "nvim_lsp" },
-						sections = { "error", "warn", "info", "hint" },
-						symbols = { error = " ", warn = " ", info = " ", hint = "" },
-					},
-				},
-			},
-		})
 	end,
 }
