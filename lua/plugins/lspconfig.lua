909cf919f83b4133e2081b9a7fbd26ebd83720a7
runharu0662 <haruta141@icloud.com>
2025-05-23 20:08:12 +0900

    hoge

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,6 +3,9 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		local lspconfig = require("lspconfig")
+        local signs = { Error = " ", Warn = " ", Hint = "󱩎 ", Info = " " }
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl })
 		lspconfig.clangd.setup({})
 	end,
 }
