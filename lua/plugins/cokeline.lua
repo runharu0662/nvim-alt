3364c91120d063cf1f745d097d7f4a12d0650e07
runharu0662 <haruta141@icloud.com>
2025-05-23 23:53:56 +0900

    hoge

--- lua/plugins/cokeline.lua
+++ lua/plugins/cokeline.lua
@@ -1,51 +1,51 @@
 return {
-	"willothy/nvim-cokeline",
-	dependencies = { "kyazdani42/nvim-web-devicons" },
-	config = function()
-		local get_hex = require("cokeline.hlgroups").get_hl_attr
+    "willothy/nvim-cokeline",
+    dependencies = { "kyazdani42/nvim-web-devicons" },
+    config = function()
+        local get_hex = require("cokeline.hlgroups").get_hl_attr
 
-		require("cokeline").setup({
-			default_hl = {
-				fg = function(buffer)
-					return buffer.is_focused and get_hex("Normal", "fg") or get_hex("Comment", "fg")
-				end,
-				bg = "NONE",
-			},
-			components = {
-				{
-					text = function(buffer)
-						return (buffer.index ~= 1) and "▏" or ""
-					end,
-					fg = function()
-						return get_hex("Normal", "fg")
-					end,
-				},
-				{
-					text = function(buffer)
-						return "    " .. buffer.devicon.icon
-					end,
-					fg = function(buffer)
-						return buffer.devicon.color
-					end,
-				},
-				{
-					text = function(buffer)
-						return buffer.filename .. "    "
-					end,
-					bold = function(buffer)
-						return buffer.is_focused
-					end,
-				},
-				{
-					text = "󰖭",
-					on_click = function(_, _, _, _, buffer)
-						buffer:delete()
-					end,
-				},
-				{
-					text = "  ",
-				},
-			},
-		})
-	end,
+        require("cokeline").setup({
+            default_hl = {
+                fg = function(buffer)
+                    return buffer.is_focused and get_hex("Normal", "fg") or get_hex("Comment", "fg")
+                end,
+                bg = "NONE",
+            },
+            components = {
+                {
+                    text = function(buffer)
+                        return (buffer.index ~= 1) and "▏" or ""
+                    end,
+                    fg = function()
+                        return get_hex("Normal", "fg")
+                    end,
+                },
+                {
+                    text = function(buffer)
+                        return "    " .. buffer.devicon.icon
+                    end,
+                    fg = function(buffer)
+                        return buffer.devicon.color
+                    end,
+                },
+                {
+                    text = function(buffer)
+                        return buffer.filename .. "    "
+                    end,
+                    bold = function(buffer)
+                        return buffer.is_focused
+                    end,
+                },
+                {
+                    text = "󰖭",
+                    on_click = function(_, _, _, _, buffer)
+                        buffer:delete()
+                    end,
+                },
+                {
+                    text = "  ",
+                },
+            },
+        })
+    end,
 }
