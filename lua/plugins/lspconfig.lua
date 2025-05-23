392825f19ec53d719ada4a9b93677c2e26712cfa
runharu0662 <haruta141@icloud.com>
2025-05-23 16:58:18 +0900

    tsserver manualy

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -5,10 +5,10 @@ return {
 		local lspconfig = require("lspconfig")
 
 		lspconfig.clangd.setup({
-			-- 必要に応じて設定を追加
 			cmd = { "clangd" },
 			filetypes = { "c", "cpp", "objc", "objcpp" },
 			root_dir = lspconfig.util.root_pattern("compile_commands.json", ".git"),
 		})
+		lspconfig.tsserver.setup({})
 	end,
 }
