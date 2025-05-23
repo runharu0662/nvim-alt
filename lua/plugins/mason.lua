cac9bcb1355280bc0bf8bfa84aa5c38a0bec7fa7
runharu0662 <haruta141@icloud.com>
2025-05-23 15:57:39 +0900

    re_fix

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -26,7 +26,7 @@ return {
 				"yamlls",
 			},
 			automatic_installation = true,
-		}),
+		})
 		require("mason-lspconfig").setup_handlers({
 			function(server_name)
 				require("lspconfig")[server_name].setup({})
