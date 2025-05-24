2c35f0ba68882e7fb78b746742d69944f866760f
runharu0662 <haruta141@icloud.com>
2025-05-24 18:04:17 +0900

    hoge

--- lua/user/ui.lua
+++ lua/user/ui.lua
@@ -23,48 +23,42 @@ vim.diagnostic.config({
 local hl = vim.api.nvim_set_hl
 local none = { bg = "none" }
 
-local function set_transparent_highlights()
-    -- 基本 UI
-    hl(0, "Normal", none)
-    hl(0, "NormalNC", none)
-    hl(0, "NormalFloat", none)
-    hl(0, "FloatBorder", none)
-    hl(0, "SignColumn", none)
-    hl(0, "EndOfBuffer", none)
-    hl(0, "StatusLine", none)
-    hl(0, "WinSeparator", none)
+-- 基本 UI
+hl(0, "Normal", none)
+hl(0, "NormalNC", none)
+hl(0, "NormalFloat", none)
+hl(0, "FloatBorder", none)
+hl(0, "SignColumn", none)
+hl(0, "EndOfBuffer", none)
+hl(0, "StatusLine", none)
+hl(0, "WinSeparator", none)
 
-    -- Pmenu / Telescope
-    hl(0, "Pmenu", none)
-    hl(0, "PmenuSel", none)
-    hl(0, "TelescopeNormal", none)
-    hl(0, "TelescopeBorder", none)
-    hl(0, "TelescopePromptNormal", none)
-    hl(0, "TelescopePromptBorder", none)
-    hl(0, "TelescopeResultsNormal", none)
-    hl(0, "TelescopeResultsBorder", none)
-    hl(0, "TelescopePreviewNormal", none)
-    hl(0, "TelescopePreviewBorder", none)
+-- Pmenu / Telescope
+hl(0, "Pmenu", none)
+hl(0, "PmenuSel", none)
+hl(0, "TelescopeNormal", none)
+hl(0, "TelescopeBorder", none)
+hl(0, "TelescopePromptNormal", none)
+hl(0, "TelescopePromptBorder", none)
+hl(0, "TelescopeResultsNormal", none)
+hl(0, "TelescopeResultsBorder", none)
+hl(0, "TelescopePreviewNormal", none)
+hl(0, "TelescopePreviewBorder", none)
 
-    -- Neo-tree
-    hl(0, "NeoTreeNormal", none)
-    hl(0, "NeoTreeNormalNC", none)
-    hl(0, "NeoTreeFloatBorder", none)
-    hl(0, "NeoTreeEndOfBuffer", none)
-    hl(0, "NeoTreeWinSeparator", none)
+-- Neo-tree
+hl(0, "NeoTreeNormal", none)
+hl(0, "NeoTreeNormalNC", none)
+hl(0, "NeoTreeFloatBorder", none)
+hl(0, "NeoTreeEndOfBuffer", none)
+hl(0, "NeoTreeWinSeparator", none)
 
-    -- LSPSaga
-    hl(0, "SagaNormal", none)
-    hl(0, "SagaBorder", none)
-    hl(0, "SagaWinbarSep", none)
+-- LSPSaga
+hl(0, "SagaNormal", none)
+hl(0, "SagaBorder", none)
+hl(0, "SagaWinbarSep", none)
 
-    -- Cokeline
-    hl(0, "CokelineBackground", none)
-    hl(0, "CokelineBufferSelected", none)
-    hl(0, "CokelineBufferVisible", none)
-    hl(0, "CokelineBuffer", none)
-end
-
-return {
-    setup = set_transparent_highlights,
-}
+-- Cokeline
+hl(0, "CokelineBackground", none)
+hl(0, "CokelineBufferSelected", none)
+hl(0, "CokelineBufferVisible", none)
+hl(0, "CokelineBuffer", none)
