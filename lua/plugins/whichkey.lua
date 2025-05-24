b2c21669fe4b05308e7b32a20ba1e31fa8a3cc08
runharu0662 <haruta141@icloud.com>
2025-05-24 22:53:32 +0900

    hgoe

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,51 +1,50 @@
 return {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-    },
-    keys = {
-        {
-            "<leader>?",
-            function()
-                require("which-key").show({ global = false })
-            end,
-            desc = "Buffer Local Keymaps (which-key)",
-        },
-    },
-    config = function()
-        local wk = require("which-key")
-        wk.add({
-            { "<leader>l", group = "git" }, -- group
-            { "<leader>f", group = "file" }, -- group
-            { "<leader>j", group = "cpp_env" }, -- group
-            { "<leader>g", group = "jump" }, -- group
-            -- { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File", mode = "n" },
-            {
-                "<leader>fb",
-                function()
-                    print("hello")
-                end,
-                desc = "Foobar",
-            },
-            {
-                "<leader>b",
-                group = "buffers",
-                expand = function()
-                    return require("which-key.extras").expand.buf()
-                end,
-            },
-            {
-                -- Nested mappings are allowed and can be added in any order
-                -- Most attributes can be inherited or overridden on any level
-                -- There's no limit to the depth of nesting
-                mode = { "n", "v" },              -- NORMAL and VISUAL mode
-                { "<leader>q", "<cmd>q<cr>",   desc = "Quit" }, -- no need to specify mode since it's inherited
-                { "<leader>w", "<cmd>w<cr>",   desc = "Write" },
-                { "<leader>Q", "<cmd>wqa<cr>", desc = "Quit All" },
-            },
-        })
-    end,
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
+	config = function()
+		local wk = require("which-key")
+		wk.add({
+			{ "<leader>l", group = "git" }, -- group
+			{ "<leader>f", group = "file" }, -- group
+			{ "<leader>j", group = "cpp_env" }, -- group
+			-- { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File", mode = "n" },
+			{
+				"<leader>fb",
+				function()
+					print("hello")
+				end,
+				desc = "Foobar",
+			},
+			{
+				"<leader>b",
+				group = "buffers",
+				expand = function()
+					return require("which-key.extras").expand.buf()
+				end,
+			},
+			{
+				-- Nested mappings are allowed and can be added in any order
+				-- Most attributes can be inherited or overridden on any level
+				-- There's no limit to the depth of nesting
+				mode = { "n", "v" }, -- NORMAL and VISUAL mode
+				{ "<leader>q", "<cmd>q<cr>", desc = "Quit" }, -- no need to specify mode since it's inherited
+				{ "<leader>w", "<cmd>w<cr>", desc = "Write" },
+				{ "<leader>Q", "<cmd>wqa<cr>", desc = "Quit All" },
+			},
+		})
+	end,
 }
