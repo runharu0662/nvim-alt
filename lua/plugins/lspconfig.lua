63a7e578a22bf7711b4b68101f0d025dc7691b36
runharu0662 <haruta141@icloud.com>
2025-05-24 11:30:06 +0900

    clangd_options

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,8 +1,17 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local lspconfig = require("lspconfig")
-		lspconfig.clangd.setup({})
-	end,
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local lspconfig = require("lspconfig")
+        lspconfig.clangd.setup({
+            cmd = {
+                "clangd",
+                "--clang-tidy",
+                "--header-insertion=never",
+                "--completion-style=detailed",
+                "--pch-storage=memory",
+                "--std=c++20",
+            },
+        })
+    end,
 }
