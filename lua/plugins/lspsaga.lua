a2c4d215ea9c8e0a1cf0b6bf79386f6c484863c7
runharu0662 <haruta141@icloud.com>
2025-05-24 13:14:21 +0900

    delete_lightbulb

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -1,10 +1,14 @@
 return {
-	"nvimdev/lspsaga.nvim",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter", -- optional
-		"nvim-tree/nvim-web-devicons", -- optional
-	},
-	config = function()
-		require("lspsaga").setup({})
-	end,
+    "nvimdev/lspsaga.nvim",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter", -- optional
+        "nvim-tree/nvim-web-devicons", -- optional
+    },
+    config = function()
+        require("lspsaga").setup({
+            lightbulb = {
+                enabe = false,
+            },
+        })
+    end,
 }
