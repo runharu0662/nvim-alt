5cb1b844bfe9d6a48bdb87e97a4b2c86d8d80868
runharu0662 <haruta141@icloud.com>
2025-05-24 10:04:00 +0900

    auto formatting save

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -2,4 +2,24 @@ return {
   "nvimtools/none-ls.nvim",
   dependencies = "nvim-lua/plenary.nvim",
   config = true,
+     local null_ls = require("null-ls")
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.prettier,
+      },
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ async = false })
+            end,
+          })
+        end
+      end,
+    })
 }
