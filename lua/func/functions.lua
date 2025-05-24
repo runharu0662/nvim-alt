766747b4b2bc732753e4fa0171fb5b9c6c1dd2df
runharu0662 <haruta141@icloud.com>
2025-05-24 23:33:10 +0900

    rounded_border

--- lua/func/functions.lua
+++ lua/func/functions.lua
@@ -1,46 +1,52 @@
 vim.api.nvim_create_user_command("RunInput", function()
-    local path = vim.fn.getcwd() .. "/stdin.txt"
+	local path = vim.fn.getcwd() .. "/stdin.txt"
 
-    -- 無理やり空ファイルで上書き作成（内容問わず上書き）
-    vim.fn.writefile({}, path)
+	-- 無理やり空ファイルで上書き作成（内容問わず上書き）
+	vim.fn.writefile({}, path)
 
-    -- horizontal split で下に開く
-    vim.cmd("split " .. path)
-    vim.cmd("resize 10") -- 高さを10行に調整（お好みで）
+	-- horizontal split で下に開く
+	vim.cmd("split " .. path)
+	vim.cmd("resize 10") -- 高さを10行に調整（お好みで）
 end, {})
 
 -- テンプレートファイルの挿入コマンド
 vim.api.nvim_create_user_command("InsTemp", function()
-    -- テンプレートファイルのパスを指定
-    local template_path = "~/.config/nvim-alt/template/base.cpp"
+	-- テンプレートファイルのパスを指定
+	local template_path = "~/.config/nvim-alt/template/base.cpp"
 
-    -- テンプレートファイルの内容を現在のバッファに挿入
-    vim.cmd("0r " .. template_path)
+	-- テンプレートファイルの内容を現在のバッファに挿入
+	vim.cmd("0r " .. template_path)
 end, {})
 
 -- 参考:https://qiita.com/nao-a/items/631bd5aa3bd20b2b3da1
 vim.opt.updatetime = 500
 
 vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(args)
-        local ft = vim.bo[args.buf].filetype
-        if ft == "NvimTree" or ft == "NeogitCommitMessage" then
-            return
-        end
+	callback = function(args)
+		local ft = vim.bo[args.buf].filetype
+		if ft == "NvimTree" or ft == "NeogitCommitMessage" then
+			return
+		end
 
-        vim.api.nvim_create_autocmd({ "CursorHold" }, {
-            buffer = args.buf,
-            callback = function()
-                vim.diagnostic.open_float(nil, { focus = false })
-                -- lspsagaのhover使うなら：
-                -- require('lspsaga.hover').render_hover_doc()
-            end,
-        })
-    end,
+		vim.api.nvim_create_autocmd({ "CursorHold" }, {
+			buffer = args.buf,
+			callback = function()
+				vim.diagnostic.open_float(nil, { focus = false })
+				-- lspsagaのhover使うなら：
+				-- require('lspsaga.hover').render_hover_doc()
+			end,
+		})
+	end,
+})
+
+-- floatwindowに枠線を追加
+vim.diagnostic.open_float(nil, {
+	focus = false,
+	border = "rounded", -- ← ここで枠線を付ける
 })
 
 vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-    callback = function()
-        vim.diagnostic.open_float(nil, { focus = false })
-    end,
+	callback = function()
+		vim.diagnostic.open_float(nil, { focus = false })
+	end,
 })
