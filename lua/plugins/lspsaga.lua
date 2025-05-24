7f95ef92c55a2384157c182781a4abfe74ee5fdc
runharu0662 <haruta141@icloud.com>
2025-05-24 23:19:57 +0900

    hoge

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -1,10 +1,19 @@
 return {
-    "nvimdev/lspsaga.nvim",
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter", -- optional
-        "nvim-tree/nvim-web-devicons",     -- optional
-    },
-    config = function()
-        require("lspsaga").setup({})
-    end,
+	"nvimdev/lspsaga.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter", -- optional
+		"nvim-tree/nvim-web-devicons", -- optional
+	},
+	config = function()
+		require("lspsaga").setup({
+			ui = {
+				-- Border type can be single, double, rounded, solid, shadow.
+				border = "rounded",
+				-- Title for finder and code action popup
+				title = true,
+				-- Set the colors of the title icon and text
+				title_icon = " ",
+			},
+		})
+	end,
 }
