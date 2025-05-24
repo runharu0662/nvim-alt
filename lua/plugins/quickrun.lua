6ee82596bd6612dd814c5a95a0ce3edcaeb7408c
runharu0662 <haruta141@icloud.com>
2025-05-24 10:42:48 +0900

    config quickrun

--- lua/plugins/quickrun.lua
+++ lua/plugins/quickrun.lua
@@ -1,7 +1,7 @@
 return {
-    "thinca/vim-quickrun",
-    config = function()
-        vim.cmd([[
+	"thinca/vim-quickrun",
+	config = function()
+		vim.cmd([[
       let g:quickrun_config = {}
 
       " C++用の設定
@@ -13,12 +13,12 @@ return {
       " デフォルトの出力設定（右に vertical split、幅は 40列くらい）
       let g:quickrun_config['_'] = {
       \  'outputter' : 'buffer',
-      \  'outputter/buffer/split' : 'rightbelow vertical',
-      \  'outputter/buffer/width' : 40,
+      \  'outputter/buffer/split' : 'leftbelow vertical',
+      \  'outputter/buffer/width' : 10,
       \  'outputter/buffer/into' : '',
       \  'outputter/buffer/close_on_empty' : 1,
       \  'hook/time/enable' : 1
       \}
     ]])
-    end,
+	end,
 }
