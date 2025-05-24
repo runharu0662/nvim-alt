05621bdfe79381b92e6ce3688de14c2aa22a3ca7
runharu0662 <haruta141@icloud.com>
2025-05-24 15:28:19 +0900

    hoge

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,34 +1,36 @@
 return {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-    },
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+	},
 
-    config = function()
-        local cmp = require("cmp")
-        local map = cmp.mapping
+	config = function()
+		local cmp = require("cmp")
+		local map = cmp.mapping
 
-        cmp.setup({
-            completion = {
-                completeopt = "menu,menuone,noinsert",
-            },
-            mapping = map.preset.insert({
-                ["<C-p>"] = map.select_prev_item(),
-                ["<C-n>"] = map.select_next_item(),
-                ["<CR>"] = map.confirm({ select = false }),
-                ["<Tab>"] = map.confirm({ select = true }),
-            }),
-            sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-            }),
-            window = {
-                completion = cmp.config.window.bordered(),
-                documentation = cmp.config.window.bordered(),
-            },
-        })
+		cmp.setup({
+			completion = {
+				completeopt = "menu,menuone,noinsert",
+			},
+			mapping = map.preset.insert({
+				["<C-p>"] = map.select_prev_item(),
+				["<C-n>"] = map.select_next_item(),
+				["<CR>"] = map.confirm({ select = false }),
+				["<Tab>"] = map.confirm({ select = true }),
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "buffer" },
+			}),
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+		})
 
-        vim.lsp.config("*", {
-            capabilities = require("cmp_nvim_lsp").default_capabilities(),
-        })
-    end,
+		vim.lsp.config("*", {
+			capabilities = require("cmp_nvim_lsp").default_capabilities(),
+		})
+	end,
 }
