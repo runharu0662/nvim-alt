4141cd326a302f075e2fe86e823cf3f545da31dd
runharu0662 <haruta141@icloud.com>
2025-05-24 11:23:19 +0900

    edit tempalte

--- lua/func/functions.lua
+++ lua/func/functions.lua
@@ -1,19 +1,19 @@
 vim.api.nvim_create_user_command("RunInput", function()
-	local path = vim.fn.getcwd() .. "/stdin.txt"
+    local path = vim.fn.getcwd() .. "/stdin.txt"
 
-	-- 無理やり空ファイルで上書き作成（内容問わず上書き）
-	vim.fn.writefile({}, path)
+    -- 無理やり空ファイルで上書き作成（内容問わず上書き）
+    vim.fn.writefile({}, path)
 
-	-- horizontal split で下に開く
-	vim.cmd("split " .. path)
-	vim.cmd("resize 10") -- 高さを10行に調整（お好みで）
+    -- horizontal split で下に開く
+    vim.cmd("split " .. path)
+    vim.cmd("resize 10") -- 高さを10行に調整（お好みで）
 end, {})
 
 -- テンプレートファイルの挿入コマンド
 vim.api.nvim_create_user_command("InsTemp", function()
-	-- テンプレートファイルのパスを指定
-	local template_path = "~/.config/nvim/template/atc.cpp"
+    -- テンプレートファイルのパスを指定
+    local template_path = "~/.config/nvim-alt/template/base.cpp"
 
-	-- テンプレートファイルの内容を現在のバッファに挿入
-	vim.cmd("0r " .. template_path)
+    -- テンプレートファイルの内容を現在のバッファに挿入
+    vim.cmd("0r " .. template_path)
 end, {})
---
+++ lua/template/base.cpp
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+
+int main() {}
