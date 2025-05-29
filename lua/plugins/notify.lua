d29f37362f8fe5e933a22b23077e5a92a1ac18ed
runharu0662 <haruta141@icloud.com>
2025-05-30 03:09:10 +0900

    re_add_notify.lua

---
+++ lua/plugins/notify.lua
@@ -0,0 +1,17 @@
+return {
+    "rcarriga/nvim-notify",
+    config = function()
+        require("notify").setup({
+            stages = "fade",
+            timeout = 3000,
+            top_down = false,
+        })
+        local telescope = require("telescope")
+        telescope.load_extension("notify")
+
+        vim.keymap.set("n", "<leader>fn", function()
+            telescope.extensions.notify.notify()
+        end)
+        vim.notify = require("notify")
+    end,
+}
