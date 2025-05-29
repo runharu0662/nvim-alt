59b79407235b6759519a718f3fcf3839e4cac173
runharu0662 <haruta141@icloud.com>
2025-05-30 02:22:29 +0900

    lspsaga_code_action

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -2,17 +2,12 @@ return {
     "nvimdev/lspsaga.nvim",
     dependencies = {
         "nvim-treesitter/nvim-treesitter", -- optional
-        "nvim-tree/nvim-web-devicons",     -- optional
+        "nvim-tree/nvim-web-devicons", -- optional
     },
     config = function()
         require("lspsaga").setup({
             diagnostic = {
                 border = "rounded",
-            },
-            lightbulb = {
-                enable = true,
-                sign = true,          -- SignColumn に表示
-                virtual_text = false, -- VirtualText を無効にすることで行がズレない
             },
         })
     end,
