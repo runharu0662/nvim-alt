dd7e4464d7c667804cb17fed310af42eda14ce4a
runharu0662 <haruta141@icloud.com>
2025-05-31 01:25:53 +0900

    add notify setting

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,20 +1,20 @@
 return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		"rcarriga/nvim-notify",
-	},
-	opts = {
-		cmdline = {
-			view = "cmdline_popup",
-		},
-		messages = {
-			view = "mini",
-			view_error = "mini",
-			view_warn = "mini",
-			view_history = "messages",
-			view_search = "virtualtext",
-		},
-	},
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
+    },
+    opts = {
+        cmdline = {
+            view = "cmdline_popup",
+        },
+        messages = {
+            view = "mini",
+            view_error = "mini",
+            view_warn = "mini",
+            view_history = "messages",
+            view_search = "virtualtext",
+        },
+    },
 }
---
+++ lua/plugins/notify.lua
@@ -0,0 +1,16 @@
+return {
+	"rcarriga/nvim-notify",
+	lazy = false,
+	config = function()
+		require("notify").setup({
+			background_colour = "#000000", -- black background to avoid transparency artifacts
+			minimum_width = 50,
+			render = "default",
+			stages = "fade_in_slide_out",
+			timeout = 3000,
+			top_down = false,
+		})
+
+		vim.notify = require("notify")
+	end,
+}
