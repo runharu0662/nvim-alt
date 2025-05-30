a8f34228ec7f6d8bd2184dd336323554dec05440
runharu0662 <haruta141@icloud.com>
2025-05-31 01:26:19 +0900

    re

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,6 +1,5 @@
 return {
 	"rcarriga/nvim-notify",
-	lazy = false,
 	config = function()
 		require("notify").setup({
 			background_colour = "#000000", -- black background to avoid transparency artifacts
