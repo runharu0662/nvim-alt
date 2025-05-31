1d653bb82ac527ce35969d8e59e632cae89576c1
runharu0662 <haruta141@icloud.com>
2025-05-31 15:03:02 +0900

    change_comment_color

--- init.lua
+++ init.lua
@@ -4,3 +4,6 @@ require("func.functions")
 require("options")
 vim.cmd([[colorscheme tokyonight-storm]])
 require("user.ui")
+
+-- change comment color
+vim.api.nvim_set_hl(0, "Comment", { fg = "#9ec6ff", italic = true })
