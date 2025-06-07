8a69b4d078a6cbc6dc00d054129b683df026dac2
runharu0662 <haruta141@icloud.com>
2025-06-07 23:34:58 +0900

    add_message

--- init.lua
+++ init.lua
@@ -5,5 +5,9 @@ require("options")
 vim.cmd([[colorscheme tokyonight-storm]])
 require("user.ui")
 
+vim.diagnostic.config({
+	virtual_text = true,
+})
+
 -- change comment color
 vim.api.nvim_set_hl(0, "Comment", { fg = "#9ec6ff", italic = true })
