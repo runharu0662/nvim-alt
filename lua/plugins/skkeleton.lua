5490d17a62a1cd242c1b082e5a983b6399ad8cae
runharu0662 <haruta141@icloud.com>
2025-06-18 18:13:32 +0900

    fixbug

--- lua/plugins/skkeleton.lua
+++ lua/plugins/skkeleton.lua
@@ -1,36 +1,18 @@
-return {
-	"vim-skk/skkeleton",
-	dependencies = { "vim-denops/denops.vim" },
-	lazy = false,
-	config = function()
-		vim.fn["skkeleton#config"]({
-			eggLikeNewline = true,
-			registerConvertResult = true,
-			globalDictionaries = { "~/.skk/SKK-JISYO.L" },
-			userDictionary = "~/.skk/skk-jisyo.user",
-			kanaTable = "rom-kana",
-			immediatelyCancel = true,
-			keepState = true,
-			showCandidatesCount = true,
-			markerHenkan = ">",
-			markerOkuri = "-",
-			indicator = {
-				hira = "[あ]",
-				kata = "[ア]",
-				hankata = "[ｱ]",
-				zenkaku = "[Ａ]",
-				abbrev = "[Aa]",
-				latin = "[A]",
-				wideLatin = "[Ａ]",
-			},
-		})
-
-		-- 候補移動（n/p）やトグル切替（C-xなど）
-		vim.fn["skkeleton#register_keymap"]("henkan", "<C-n>", "nextCandidate")
-		vim.fn["skkeleton#register_keymap"]("henkan", "<C-p>", "prevCandidate")
-
-		-- SKK起動トグル（<C-x>推奨、<C-j>は衝突注意）
-		vim.keymap.set("i", "<C-x>", "<Plug>(skkeleton-toggle)")
-		vim.keymap.set("c", "<C-x>", "<Plug>(skkeleton-toggle)")
-	end,
-}
+vim.fn["skkeleton#config"]({
+	eggLikeNewline = true,
+	registerConvertResult = true,
+	globalDictionaries = { "~/.skk/SKK-JISYO.L" },
+	userDictionary = "~/.skk/skk-jisyo.user",
+	kanaTable = "rom-kana",
+	immediatelyCancel = true,
+	keepState = true,
+	showCandidatesCount = true,
+	indicator = {
+		hira = "[あ]",
+		kata = "[ア]",
+		zenkaku = "[Ａ]",
+		abbrev = "[Aa]",
+		latin = "[A]",
+		wideLatin = "[Ａ]",
+	},
+})
