f7e0d246161bf7d11e90e6bc80b9bb21b448e726
runharu0662 <haruta141@icloud.com>
2025-10-02 10:54:50 +0900

    mapping_copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -6,15 +6,46 @@ return {
 		require("copilot").setup({
 			suggestion = {
 				enabled = true,
-				auto_trigger = false,
+				auto_trigger = false, -- trueにすると自動で候補が出る
 			},
 			panel = {
 				enabled = true,
 			},
-
 			filetypes = {
-				["*"] = true,
+				["*"] = true, -- すべてのファイルタイプで有効
 			},
 		})
+
+		-- Copilot suggestion 操作用キーマップ
+		local s = require("copilot.suggestion")
+
+		-- 候補をすべて受け入れる
+		vim.keymap.set("i", "<C-l>", function()
+			s.accept()
+		end, { desc = "Copilot: Accept" })
+
+		-- 単語だけ受け入れる
+		vim.keymap.set("i", "<C-;>", function()
+			s.accept_word()
+		end, { desc = "Copilot: Accept Word" })
+
+		-- 行だけ受け入れる
+		vim.keymap.set("i", "<C-'>", function()
+			s.accept_line()
+		end, { desc = "Copilot: Accept Line" })
+
+		-- 複数候補がある場合の切り替え
+		vim.keymap.set("i", "<C-]>", function()
+			s.next()
+		end, { desc = "Copilot: Next Suggestion" })
+
+		vim.keymap.set("i", "<C-[>", function()
+			s.prev()
+		end, { desc = "Copilot: Previous Suggestion" })
+
+		-- 候補を閉じる
+		vim.keymap.set("i", "<C-x>", function()
+			s.dismiss()
+		end, { desc = "Copilot: Dismiss" })
 	end,
 }
