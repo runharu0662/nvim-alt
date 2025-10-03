cd11f11a1d95db670de7f44b6c0f994456c3c97d
runharu <138078013+runharu0662@users.noreply.github.com>
2025-10-03 12:06:25 +0900

    Dev (#11)
    * mapping_copilot
    
    * fix_mapping
    
    * fix_syntax_err
    
    * change_to_auto_trigger
    
    * rere_mapping

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,24 +1,21 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  config = function()
-    require("copilot").setup({
-      suggestion = {
-        enabled = true,
-        auto_trigger = true, -- 自動ポップ無効
-        keymap = {
-          accept = "<C-l>",      -- 候補を確定
-          accept_word = "<M-;>", -- 単語だけ確定
-          accept_line = "<M-'>", -- 行だけ確定
-          next = "<M-n>",        -- 次の候補
-          prev = "<M-p>",        -- 前の候補
-          dismiss = "<C-x>",     -- 候補を閉じる
-        },
-      },
-      panel = { enabled = true },
-      filetypes = { ["*"] = true },
-    })
-    
-  end,
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	config = function()
+		require("copilot").setup({
+			suggestion = {
+				enabled = true,
+				auto_trigger = true, 
+				keymap = {
+					accept = "<C-l>", -- 候補を確定
+					next = "<M-n>", -- 次の候補
+					prev = "<M-p>", -- 前の候補
+					dismiss = "<C-]>",
+				},
+			},
+			panel = { enabled = true },
+			filetypes = { ["*"] = true },
+		})
+	end,
 }
