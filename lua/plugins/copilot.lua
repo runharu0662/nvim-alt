ed24abba4de000c02512be40ca66dedab846a6f9
runharu0662 <haruta141@icloud.com>
2025-10-16 11:21:10 +0900

    change_opacity_1.0

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -6,12 +6,12 @@ return {
 		require("copilot").setup({
 			suggestion = {
 				enabled = true,
-				auto_trigger = true, 
+				auto_trigger = true,
 				keymap = {
 					accept = "<C-l>", -- 候補を確定
 					next = "<M-n>", -- 次の候補
 					prev = "<M-p>", -- 前の候補
-					dismiss = "<C-]>",
+					dismiss = "<C-]>", -- 候補をキャンセル
 				},
 			},
 			panel = { enabled = true },
