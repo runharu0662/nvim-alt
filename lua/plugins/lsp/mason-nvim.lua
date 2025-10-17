a12f4d520f04765ce97ff8316e46bc525d49f54a
runharu0662 <haruta141@icloud.com>
2025-10-17 10:11:27 +0900

    revice_err?

--- lua/plugins/lsp/mason-nvim.lua
+++ lua/plugins/lsp/mason-nvim.lua
@@ -2,17 +2,28 @@ return {
 	"williamboman/mason.nvim",
 	dependencies = {
 		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
+		"neovim/nvim-lspconfig", -- ← これが重要
 	},
+	build = ":MasonUpdate",
+	event = { "BufReadPre", "BufNewFile" },
 	config = function()
-		require("mason").setup({
-			ui = {
-				check_outdated_packages_on_open = false,
-				border = "single",
-			},
+		local mason_ok, mason = pcall(require, "mason")
+		if not mason_ok then
+			vim.notify("mason.nvim not found", vim.log.levels.ERROR)
+			return
+		end
+
+		mason.setup({
+			ui = { border = "single", check_outdated_packages_on_open = false },
 		})
 
-		require("mason-lspconfig").setup({
+		local mason_lsp_ok, mason_lsp = pcall(require, "mason-lspconfig")
+		if not mason_lsp_ok then
+			vim.notify("mason-lspconfig not found", vim.log.levels.ERROR)
+			return
+		end
+
+		mason_lsp.setup({
 			ensure_installed = {
 				"lua_ls",
 				"pyright",
@@ -23,22 +34,9 @@ return {
 				"bashls",
 				"yamlls",
 				"rust_analyzer",
+				"clangd",
 			},
+			automatic_installation = true,
 		})
-
-		--------------------------
-		-- new lspconfig setup
-		local lsp = vim.lsp.config
-		-------------------------
-
-		lsp.lua_ls.setup({})
-		lsp.pyright.setup({})
-		lsp.jsonls.setup({})
-		lsp.html.setup({})
-		lsp.cssls.setup({})
-		lsp.gopls.setup({})
-		lsp.bashls.setup({})
-		lsp.yamlls.setup({})
-		lsp.rust_analyzer.setup({})
 	end,
 }
--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -2,6 +2,45 @@ return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
-		vim.lsp.config.clangd.setup({})
+		local ok, lspconfig = pcall(require, "lspconfig")
+		if not ok then
+			vim.notify("nvim-lspconfig not found", vim.log.levels.ERROR)
+			return
+		end
+
+		-- LSP server setup
+		local servers = {
+			"lua_ls",
+			"pyright",
+			"jsonls",
+			"html",
+			"cssls",
+			"gopls",
+			"bashls",
+			"yamlls",
+			"rust_analyzer",
+			"clangd",
+		}
+
+		for _, server in ipairs(servers) do
+			local opts = {}
+
+			if server == "lua_ls" then
+				opts = {
+					settings = {
+						Lua = {
+							diagnostics = { globals = { "vim" } },
+							workspace = { checkThirdParty = false },
+						},
+					},
+				}
+			end
+
+			if lspconfig[server] then
+				lspconfig[server].setup(opts)
+			else
+				vim.notify("Server not found: " .. server, vim.log.levels.WARN)
+			end
+		end
 	end,
 }
