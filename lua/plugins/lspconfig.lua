f2e7f43a4852d99b6f95edcf20d8cbba4b6f6924
runharu0662 <haruta141@icloud.com>
2025-10-17 09:28:16 +0900

    changed_lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,7 +2,6 @@ return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
-		local lspconfig = require("lspconfig")
-		lspconfig.clangd.setup({})
+		vim.lsp.config.clangd.setup({})
 	end,
 }
