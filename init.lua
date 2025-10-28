b66d85b6e1e17a486162bd3264bde63c6965099f
runharu0662 <haruta141@icloud.com>
2025-10-28 15:26:05 +0900

    fix-path

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@
 -- Please configure your personal settings here.
 
 -- Path to your Obsidian vault for image pasting.
-vim.g.obsidian_vault_path = vim.fn.expand("~/Documents/Latest_Documents/Obsidian_Vaults/obsidian-vault1/Obsidian_Vault1")
+vim.g.obsidian_vault_path = vim.fn.expand("~/Documents/Obsidian_Vaults/Obsidian_Vault1")
 
 -- ===================================================================
 
