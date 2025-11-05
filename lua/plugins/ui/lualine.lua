0f16023fecce20bb8ee89424543a3e655af73975
HARUTA ISHII <runharu@runbook.local>
2025-11-05 16:01:59 +0900

    change_lualine_theme

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -5,91 +5,71 @@ local M = {
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
 	},
-	opts = {
-		options = {
-			theme = "dracula",
-			globalstatus = true,
-		},
-		sections = {
-			lualine_b = {
-				{
-					"branch",
-					icon = icons.branch_icon,
-					color = function()
-						return { gui = "bold" } -- ⛔ カラーを固定せず関数で遅延評価（テーマによる上書きを防ぐ）
-					end,
-				},
-				{
-					"diagnostics",
-					symbols = {
-						error = icons.error_icon,
-						warn = icons.warn_icon,
-						hint = icons.hint_icon,
-						info = icons.info_icon,
-					},
-				},
-			},
-			lualine_c = {
-				{
-					"diff",
-					symbols = {
-						added = icons.added_icon,
-						modified = icons.modified_icon,
-						removed = icons.removed_icon,
-					},
-				},
-				{
-					function()
-						local file = vim.fn.expand("%:t")
-						if file == "" then
-							return "[No Name]"
-						end
-						local icon = icons.code_icon .. file
-						if vim.bo.modified then
-							icon = icon .. " " .. icons.modified_icon
-						end
-						if vim.bo.readonly then
-							icon = icon .. " " .. icons.readonly_icon
-						end
-						return icon
-					end,
-					color = function()
-						return { fg = "#f9e2af", gui = "bold" } -- 遅延評価で上書きを防ぐ
-					end,
-				},
-				{
-					"filetype",
-					color = function()
-						return { fg = "#f5c2e7", gui = "bold" }
-					end,
-				},
-			},
-			lualine_x = {
-				{
-					"encoding",
-					color = function()
-						return { fg = "#f5c2e7", gui = "bold" }
-					end,
-				},
-				{
-					function()
-						local fmt = vim.bo.fileformat
-						if fmt == "unix" then
-							return icons.linux_icon
-						elseif fmt == "dos" then
-							return icons.win_icon
-						elseif fmt == "mac" then
-							return icons.mac_icon
-						end
-					end,
-					color = function()
-						return { fg = "#89b4fa" }
-					end,
-				},
-			},
-		},
-	},
 	event = "VeryLazy",
+    config = function()
+        -- Bubbles config for lualine
+        -- Author: lokesh-krishna
+        -- MIT license, see LICENSE for more details.
+
+        -- stylua: ignore
+        local colors = {
+          blue   = '#80a0ff',
+          cyan   = '#79dac8',
+          black  = '#080808',
+          white  = '#c6c6c6',
+          red    = '#ff5189',
+          violet = '#d183e8',
+          grey   = '#303030',
+        }
+
+        local bubbles_theme = {
+          normal = {
+            a = { fg = colors.black, bg = colors.violet },
+            b = { fg = colors.white, bg = colors.grey },
+            c = { fg = colors.white },
+          },
+
+          insert = { a = { fg = colors.black, bg = colors.blue } },
+          visual = { a = { fg = colors.black, bg = colors.cyan } },
+          replace = { a = { fg = colors.black, bg = colors.red } },
+
+          inactive = {
+            a = { fg = colors.white, bg = colors.black },
+            b = { fg = colors.white, bg = colors.black },
+            c = { fg = colors.white },
+          },
+        }
+
+        require('lualine').setup {
+          options = {
+            theme = bubbles_theme,
+            component_separators = '',
+            section_separators = { left = '', right = '' },
+          },
+          sections = {
+            lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+            lualine_b = { 'filename', 'branch' },
+            lualine_c = {
+              '%=', --[[ add your center components here in place of this comment ]]
+            },
+            lualine_x = {},
+            lualine_y = { 'filetype', 'progress' },
+            lualine_z = {
+              { 'location', separator = { right = '' }, left_padding = 2 },
+            },
+          },
+          inactive_sections = {
+            lualine_a = { 'filename' },
+            lualine_b = {},
+            lualine_c = {},
+            lualine_x = {},
+            lualine_y = {},
+            lualine_z = { 'location' },
+          },
+          tabline = {},
+          extensions = {},
+        }
+    end,
 }
 
 return M
